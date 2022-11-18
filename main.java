a625be3c49ff83b9d7da2aafb10821fb48c67b32
DongyangLi6816 <dongyangli6816@gmail.com>
2022-11-18 13:28:41 -0800

    Created the window of the game

---
+++ main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setTitle("勇者斗恶龙");
+
+        GamePanel game_panel = new GamePanel();
+        window.add(game_panel);
+
+        window.pack();//adjust the window to preferred size
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        game_panel.startGameThread();
+
+    }
+}
